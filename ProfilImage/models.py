81e2f75ba9d7e8d444d65a72eeee63a5bfc34a9d
Dan <atibudan2@gmail.com>
2022-06-05 01:57:30 +0200

    add signals method

--- ProfilImage/models.py
+++ ProfilImage/models.py
@@ -1,3 +1,5 @@
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
 from django.db import models
 import datetime
 import random
@@ -11,14 +13,24 @@ def create_dynamic_path(instance, filename):
     name += ''.join([random.choice(string.ascii_letters)
                     for _ in range(0, 15)])
     date = datetime.datetime.now().strftime(f"%Y-%m-%d/{f'{name}.{ext}'}")
+    print("DATE : ", date)
     return date
 
 
 class Citizen(models.Model):
-    Image = models.ImageField(
-        upload_to=create_dynamic_path, null=False, blank=False)
+    Image = models.ImageField(upload_to=create_dynamic_path, null=False, blank=False)
     CreatedAt = models.DateTimeField(auto_now_add=True)
     UserId = models.IntegerField(null=True)
 
     def Json(self):
         return {'id': self.id, 'image': self.Image.url, 'user_id': self.UserId}
+
+
+
+@receiver(pre_save, sender=Citizen)
+def PreSaveSignal(**kwargs):
+    print("PRE SAVE : ", kwargs)
+
+@receiver(post_save, sender=Citizen)
+def PostSaveSignal(**kwargs):
+    print("POST SAVED : ", kwargs)
